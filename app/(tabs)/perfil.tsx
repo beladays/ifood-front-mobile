4853bfaa7b70338749c19fde73685af15d31f92c
DafniMilla <dafni8663@gmail.com>
2025-11-09 23:57:35 -0300

    tela de perfil

--- app/(tabs)/perfil.tsx
+++ app/(tabs)/perfil.tsx
@@ -1,9 +1,140 @@
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, ActivityIndicator, StyleSheet, Alert } from "react-native";
+import { Avatar, Button, Card } from "@rneui/themed";
+import axios from "axios";
+import { ListItem } from '@rneui/themed';
+import { Icon } from "@rneui/themed";
 
-export default function PerfilScreen() {
+
+export default function Perfil() {
+  const [user, setUser] = useState<{ name: string; email: string; avatar: string } | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function carregarPerfil() {
+      try {
+        //  API:
+        // const token = "seu_token_aqui";
+        // const response = await axios.get("https://suaapi.com/profile", {
+        //   headers: { Authorization: `Bearer ${token}` },
+        // });
+        // const data = response.data.user;
+
+        // Exemplo sem token (API pública do Random User) remover depois
+        const response = await axios.get("https://randomuser.me/api/");
+        const data = response.data.results[0]; // acessa o primeiro usuário retornado
+
+
+
+        // Define o usuário com base nos dados da API
+        setUser({
+          name: `${data.name.first} ${data.name.last}`,
+          email: data.email,
+          avatar: data.picture.large,
+        });
+      } catch (error) {
+        console.error("Erro ao carregar perfil:", error);
+        Alert.alert("Erro", "Não foi possível carregar o perfil.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    carregarPerfil();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#E91E63" />
+        <Text>Carregando perfil...</Text>
+      </View>
+    );
+  }
+
+  if (!user) {
+    return (
+      <View style={styles.center}>
+        <Text>Erro ao carregar dados do usuário </Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Tela de Perfil</Text>
+    <View style={styles.container}>
+      <Card containerStyle={styles.card}>
+        <View style={styles.avatarContainer}>
+          <Avatar
+            size={100}
+            rounded
+            source={{ uri: user.avatar }}
+            containerStyle={{ marginBottom: 15 }}
+          />
+          <Text style={styles.name}>{user.name}</Text>
+          <Text style={styles.email}>{user.email}</Text>
+        </View>
+
+        <Button
+          title="Sair"
+          onPress={() => Alert.alert("Logout realizado!")}
+          buttonStyle={{ backgroundColor: "#E91E63", borderRadius: 8 }}
+          containerStyle={{ marginTop: 20 }}
+        />
+      </Card>
+
+      <>
+        <ListItem>
+          <Icon name="location-on"
+            type="material"
+            color="#E91E63"
+            size={28}
+          />
+
+          <ListItem.Content>
+            <ListItem.Title>Endereço</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+
+        <ListItem>
+          <Icon name="trash-can-outline" type="material-community" color="grey" />
+          <ListItem.Content>
+            <ListItem.Title>Trash</ListItem.Title>
+          </ListItem.Content>
+          <ListItem.Chevron />
+        </ListItem>
+      </>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#f82727ff",
+  },
+  card: {
+    borderRadius: 10,
+    padding: 20,
+    shadowOpacity: 0.1,
+    elevation: 5,
+  },
+  avatarContainer: { alignItems: "center" },
+  name: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  email: {
+    fontSize: 16,
+    color: "#555",
+    marginTop: 4,
+  },
+});
