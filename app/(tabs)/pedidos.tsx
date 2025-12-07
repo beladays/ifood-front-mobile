8c8a0bd501e57d824a0e8bdb1fb922b6d6a7fbde
DafniMilla <dafni8663@gmail.com>
2025-12-07 14:33:43 -0300

    add estilo na tela de pedidos

--- app/(tabs)/pedidos.tsx
+++ app/(tabs)/pedidos.tsx
@@ -2,7 +2,7 @@ import { Card } from "@rneui/themed";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-
+import { Link } from "expo-router";
 type Pedido = {
   id: number;
   valorTotal: number;
@@ -52,9 +52,10 @@ export default function Pedidos() {
       try {
         const token = localStorage.getItem("token");
         const response = await axios.get("http://localhost:8081/pedidos/historico/cliente", {
-        headers: {Authorization: `Bearer ${token}`,
-        },
-      });
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         setPedidos(response.data);
       } catch (error) {
         console.error("Erro ao carregar pedidos:", error);
@@ -67,54 +68,73 @@ export default function Pedidos() {
   }, []);
 
   return (
-   <View style={styles.container}>
+    <View style={styles.container}>
 
-  <Text style={styles.subtitulo}>Histórico de pedidos</Text>
+      <View style={styles.cabecalho}>
+    <Text style={styles.cabecalhoTitulo}>Histórico de pedidos</Text>
+  </View>
 
-  {loading ? (
-    <ActivityIndicator size="large" color="#ff4d4d" style={{ marginTop: 20 }} />
-  ) : pedidos.length === 0 ? (
-    <Text style={styles.nenhum}>Nenhum pedido encontrado.</Text>
-  ) : (
-    pedidos.map((item) => (
-      <Card key={item.id} containerStyle={styles.card}>
-        
-        {/* Nome do restaurante */}
-        <Text style={styles.cardTitulo}>
-          Restaurante: {item.restaurante.nome}
-        </Text>
+      {loading ? (
+        <ActivityIndicator size="large" color="#ff4d4d" style={{ marginTop: 20 }} />
+      ) : pedidos.length === 0 ? (
 
-        {/* Valor total */}
-        <Text style={styles.cardDesc}>
-          Total: R$ {item.valorTotal.toFixed(2)}
-        </Text>
+        <View style={styles.containertext}>
+          <Text style={styles.nenhum}>
+            Poxa, você não tem nenhum pedido {"\n"}
+          </Text>
 
-        {/* Status */}
-        <Text style={styles.cardDesc}>
-          Status: {item.status}
-        </Text>
+          <Text style={styles.nenhumsub}>
+            Seus pedidos vão aparecer aqui quando você fizer.
+            Que tal aproveitar as ofertas pra pedir agora? {"\n"}{"\n"}
+          </Text>
 
-        {/* Data formatada */}
-        <Text style={styles.cardDesc}>
-          Data: {new Date(item.dataCriacao).toLocaleString()}
-        </Text>
-
-        {/* Lista de itens */}
-        <Text style={[styles.cardTitulo, { marginTop: 10 }]}>Itens:</Text>
-
-        {item.itens.map((i) => (
-          <View key={i.id} style={{ marginBottom: 6 }}>
-            <Text style={styles.cardDesc}>
-              • {i.quantidade}x {i.produto.nome} — R$ {i.subtotal.toFixed(2)}
+          <Link href="/">
+            <Text style={styles.nenhumlink}>
+              Fazer pedido
             </Text>
-          </View>
-        ))}
+          </Link>
+        </View>
 
-      </Card>
-    ))
-  )}
+      ) : (
+        pedidos.map((item) => (
+          <Card key={item.id} containerStyle={styles.card}>
 
-</View>
+            {/* Nome do restaurante */}
+            <Text style={styles.cardTitulo}>
+              Restaurante: {item.restaurante.nome}
+            </Text>
+
+            {/* Valor total */}
+            <Text style={styles.cardDesc}>
+              Total: R$ {item.valorTotal.toFixed(2)}
+            </Text>
+
+            {/* Status */}
+            <Text style={styles.cardDesc}>
+              Status: {item.status}
+            </Text>
+
+            {/* Data formatada */}
+            <Text style={styles.cardDesc}>
+              Data: {new Date(item.dataCriacao).toLocaleString()}
+            </Text>
+
+            {/* Lista de itens */}
+            <Text style={[styles.cardTitulo, { marginTop: 10 }]}>Itens:</Text>
+
+            {item.itens.map((i) => (
+              <View key={i.id} style={{ marginBottom: 6 }}>
+                <Text style={styles.cardDesc}>
+                  • {i.quantidade}x {i.produto.nome} — R$ {i.subtotal.toFixed(2)}
+                </Text>
+              </View>
+            ))}
+
+          </Card>
+        ))
+      )}
+
+    </View>
 
   );
 }
@@ -122,7 +142,7 @@ export default function Pedidos() {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
-    backgroundColor: "#fff",
+    backgroundColor: "#f5f5f5ff",
     flex: 1,
   },
   subtitulo: {
@@ -132,12 +152,28 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 16,
     textAlign: "center",
+   
   },
   nenhum: {
-    color: "#777",
-    marginTop: 400,
+    color: "#000000ff",
+    marginTop: 200,
     textAlign: "center",
     fontSize: 16,
+    fontWeight: "700",
+  },
+  nenhumsub:{
+  textAlign: "center",
+  color:"#6e6d6dff"
+  },
+  nenhumlink:{
+      textAlign: "center",
+      color: "#ff0000ff",
+      fontWeight: "700",
+
+  },
+  containertext:{
+textAlign:"center",
+justifyContent: "center"
   },
   card: {
     borderRadius: 14,
@@ -154,5 +190,25 @@ const styles = StyleSheet.create({
   cardDesc: {
     fontSize: 14,
     color: "#666",
+  },
+    cabecalho: {
+    height: 120, 
+    backgroundColor: "#d91f26", 
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    justifyContent: "flex-end", // título na parte inferior
+    paddingBottom: 16,
+    paddingHorizontal: 16,
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  cabecalhoTitulo: {
+    fontSize: 22,
+    fontWeight: "700",
+    color: "#fff",
+    textAlign: "center",
   },
 });
